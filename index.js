614312d1c92f8979938041ce8e4142511a657d4b
GuptaRohit2001 <imrohitgupta2001@gmail.com>
2025-08-24 23:38:24 +0530

    authentication added

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const path = require("path");
 const mongoose=require('mongoose');
-
+const cookieParser=require('cookie-parser');
 const userRoute=require('./routes/user');
+const { checkForAuthenticationCookie } = require("./middleware/authentication");
 
 const app = express();
 const PORT = 8000;
@@ -14,10 +15,13 @@ app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
 app.use(express.urlencoded({ extended: false }));
-
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie('token'));
 // routes
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home",{
+    user: req.user,
+  });
 });
 
 app.use("/user",userRoute)
