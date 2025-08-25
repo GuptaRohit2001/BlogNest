7c8a4381b443b97f8133f7889d6feca4ff522ef6
GuptaRohit2001 <imrohitgupta2001@gmail.com>
2025-08-25 09:11:17 +0530

    blog part done

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require("path");
 const mongoose=require('mongoose');
 const cookieParser=require('cookie-parser');
 const userRoute=require('./routes/user');
+const blogRoute=require('./routes/blog');
+const Blog=require('./models/blog');
 const { checkForAuthenticationCookie } = require("./middleware/authentication");
 require("dotenv").config();
 
@@ -18,13 +20,17 @@ app.set("views", path.resolve("./views"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie('token'));
+app.use(express.static(path.resolve('./public')));
 // routes
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  const allBlogs=await Blog.find({});
   res.render("home",{
     user: req.user,
+    blogs: allBlogs,
   });
 });
 
 app.use("/user",userRoute)
+app.use("/blog",blogRoute)
 
 app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}`));
